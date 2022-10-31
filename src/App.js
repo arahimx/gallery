a821937f3c440fcbe3b48a0992aac0ccf9e39d80
Abdur Rahim <arawan0316@gmail.com>
2022-11-01 00:10:58 +0300

    show random images and videos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import data from "./data/data";
 
-function App() {
+export default function App() {
+  useEffect(() => {
+    filterGallery();
+  }, []);
+
+  const [gallery, setGallery] = useState(data);
+  const filterGallery = () => {
+    const max = data.length;
+    const id = Math.floor(Math.random() * max + 1);
+    const result = data.filter((curData) => {
+      return curData.id === id;
+    });
+    setGallery(result);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="contaner">
+        <div className="container">
+          {gallery.map((values) => {
+            const { id, title, alt, url, fileType } = values;
+            return (
+              <>
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="card text-center col-md-4 mt-3 col-sm-10 p-0">
+                      <div class="card-header p-0" id={id}>
+                        {(function () {
+                          if (fileType === "video" || fileType === "mp4") {
+                            return (
+                              <video width="auto" height="240" controls>
+                                <source src={url} type="video/mp4"/>
+                              </video>
+                            );
+                          } else if (
+                            fileType === "image" ||
+                            fileType === "images" ||
+                            fileType === "img" ||
+                            fileType === "jpg" ||
+                            fileType === "png" ||
+                            fileType === "jpeg"
+                          ) {
+                            return (
+                              <img src={url} alt={alt} class="w-100" />
+                            );
+                          }
+                        })()}
+                      </div>
+                      <div class="card-body">
+                        <h5 class="card-title">{title}</h5>
+                      </div>
+                      <div class="card-footer text-muted">
+                        {" "}
+                        <a
+                          type="button"
+                          className="btn btn-success text-white"
+                          onClick={filterGallery}
+                        >
+                          Change Image
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </section>
+    </>
   );
 }
-
-export default App;
---
+++ src/data/data.js
@@ -0,0 +1,60 @@
+const Data = [
+    //   D20s
+    {
+      id: 1,
+      title: "image 1",
+      alt: "image 1",
+      url: "/assets/data/1.jpg",
+      fileType: "img",
+
+    },
+    {
+        id: 2,
+        title: "image 2",
+        alt: "image 2",
+        url: "/assets/data/2.jpg",
+        fileType: "img",
+  
+      },
+      {
+        id: 3,
+        title: "image 3",
+        alt: "image 3",
+        url: "/assets/data/3.jpg",
+        fileType: "img",  
+      },
+      {
+        id: 4,
+        title: "image 4",
+        alt: "image 4",
+        url: "/assets/data/4.jpg",
+        fileType: "img",
+  
+      },        {
+        id: 5,
+        title: "image 5",
+        alt: "image3",
+        url: "/assets/data/3.jpg",
+        fileType: "img",
+  
+      },
+      {
+        id: 6,
+        title: "image 6",
+        alt: "image 4",
+        url: "/assets/data/4.jpg",
+        fileType: "img",
+  
+      },  
+      {
+        id: 7,
+        title: "video ",
+        alt: "image4",
+        url: "/assets/data/4.mp4",
+        fileType: "mp4",
+  
+      },  
+  
+  ];
+  export default Data;
+  
